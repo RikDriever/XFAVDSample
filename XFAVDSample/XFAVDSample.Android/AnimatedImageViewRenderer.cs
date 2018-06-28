b9de5564f427a7e50a4bbf9465de52b2b7d652dd
RikD <rik@instorebroadcast,com>
2018-06-28 13:49:41 +0200

    Initial commit

---
+++ XFAVDSample/XFAVDSample.Android/AnimatedImageViewRenderer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using XFAVDSample.Droid;
+using XFAVDSample.Views;
+
+[assembly: ExportRenderer(typeof(AnimatedImageView), typeof(AnimatedImageViewRenderer))]
+namespace XFAVDSample.Droid
+{
+    class AnimatedImageViewRenderer : ProgressBarRenderer
+    {
+        public AnimatedImageViewRenderer(Context context) : base(context)
+        {
+
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.ProgressBar> e)
+        {
+            base.OnElementChanged(e);
+            CreateAnimatedImageView((AnimatedImageView)e.NewElement);
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+        }
+
+        private void CreateAnimatedImageView(AnimatedImageView animatedImageView)
+        {
+            if (Control != null)
+            {
+                var progressBar = Control as Android.Widget.ProgressBar;
+                progressBar.IndeterminateDrawable = Context.GetDrawable(animatedImageView.SourceUrl);
+                progressBar.Indeterminate = true;
+            }
+        }
+
+        protected override void OnLayout(bool changed, int l, int t, int r, int b)
+        {
+            base.OnLayout(changed, l, t, r, b);
+        }
+
+    }
+}
---
+++ XFAVDSample/XFAVDSample/Views/AnimatedImageView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace XFAVDSample.Views
+{
+    public class AnimatedImageView : ProgressBar
+    {
+        public static readonly BindableProperty SourceUrlProperty = BindableProperty.Create(
+            propertyName: nameof(SourceUrl),
+            returnType: typeof(string),
+            declaringType: typeof(AnimatedImageView));
+        public string SourceUrl
+        {
+            get => (string)GetValue(SourceUrlProperty);
+            set => SetValue(SourceUrlProperty, value);
+        }
+    }
+}
